782f46d15a597658e79f465b526ff2f0e0fab741
Rodrigo <rodrigo.arcoverdi@gmail.com>
2022-08-05 14:25:13 +0200

    Change package description
    It will be needed to be further changed even more

--- R/RepertoiR.R
+++ R/RepertoiR.R
@@ -1,8 +1,7 @@
 #' B cell receptor immune repertoire quality control and analysis
 #'
 #' RepertoiR was created to do quality control of VDJ sequencing,
-#' combining single-cell sorted data and specificity with sanguer sequences.
-#' In addition, it also includes analysis tools of VDJ sequences from standard AIRR-compliant datasets.
+#' combining single-cell sorted data (FACS) and specificity with sanguer sequence information.
 #'
 #'
 #' @docType package
