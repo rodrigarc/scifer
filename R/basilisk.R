438b9a9274344572da18cb5f2c25c063b0baab15
Matt <mjhinchcliff@gmail.com>
2024-06-27 16:43:03 +0200

    Function for IgBlast
    Added function for igblast via basilisk
    Arguments for the function paths to database and fasta files.
    Needs updated markdown comments describing the function
    Needs function optimization

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,6 +1,4 @@
-#' @import basilisk
-
-
+#' @importFrom basilisk BasiliskEnvironment
 env <- BasiliskEnvironment(envname="igblast_wrap_basilisk",
                               pkgname = "scifer",
                               packages=c("python=3.9", "dnaio==1.2.1", "igblast==1.22.0")
