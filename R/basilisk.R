b0ca672f66a9e364dae775f0b45c9641552b2a87
Matt <mjhinchcliff@gmail.com>
2024-07-04 14:33:51 +0200

    Update dependencies for basilisk
    - building and R CMD check working on macOS apple and intel processors

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,5 +1,10 @@
 #' @importFrom basilisk BasiliskEnvironment
 env <- BasiliskEnvironment(envname="igblast_wrap_basilisk",
                               pkgname = "scifer",
-                              packages=c("python=3.9", "dnaio==1.2.1", "igblast==1.22.0")
+                              packages=c("python==3.9.19", 
+                                         "dnaio==1.2.1", 
+                                         "igblast==1.22.0"
+                                         ),
+                              channels= c("bioconda", "conda-forge")
+                           
 )
