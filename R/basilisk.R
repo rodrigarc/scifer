fe24a4c4f36b875ec7edd46ebe0b4d5f71049981
Matt <mjhinchcliff@gmail.com>
2024-07-08 16:38:16 +0200

    Updated Environment Dependencies
    - Additional full dependencies for basilisk python environment:
            "cffi==1.16.0",
            "isal==1.6.1",
            "pip==24.0",
            "pycparser==2.22",
            "setuptools==70.1.1",
            "wheel==0.43.0",
            "xopen==2.0.1",
            "zlib-ng==0.4.3",
            "zstandard==0.22.0"

--- R/basilisk.R
+++ R/basilisk.R
@@ -5,7 +5,16 @@ env <- BasiliskEnvironment(
     packages = c(
         "python==3.9.19",
         "dnaio==1.2.1",
-        "igblast==1.22.0"
+        "igblast==1.22.0",
+        "cffi==1.16.0",
+        "isal==1.6.1",
+        "pip==24.0",
+        "pycparser==2.22",
+        "setuptools==70.1.1",
+        "wheel==0.43.0",
+        "xopen==2.0.1",
+        "zlib-ng==0.4.3",
+        "zstandard==0.22.0"
     ),
     channels = c("bioconda", "conda-forge")
 )
