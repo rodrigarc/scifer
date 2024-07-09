73354507b3d4dad638e5eec6e13e2b11ed6c69d8
Matt <mjhinchcliff@gmail.com>
2024-07-09 11:57:37 +0200

    Add pip dependency to env
    -- Add pip=24.0 dependency to basilisk env

--- R/basilisk.R
+++ R/basilisk.R
@@ -7,7 +7,8 @@ env <- BasiliskEnvironment(
         "dnaio==1.2.1",
         "igblast==1.22.0",
         "cffi==1.16.0",
-        "python-isal==1.6.1"
+        "python-isal==1.6.1",
+        "pip==24.0"
     ),
     channels = c("bioconda", "conda-forge")
 )
