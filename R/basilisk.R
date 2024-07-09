384c0acc0047c054eb5f9d04cb125662bd876bfa
Matt <mjhinchcliff@gmail.com>
2024-07-09 11:48:43 +0200

    Adding isal to env dependency
    - Add isal==1.6.1 dependency to basilisk env

--- R/basilisk.R
+++ R/basilisk.R
@@ -6,7 +6,8 @@ env <- BasiliskEnvironment(
         "python==3.9.19",
         "dnaio==1.2.1",
         "igblast==1.22.0",
-        "cffi==1.16.0"
+        "cffi==1.16.0",
+        "isal==1.6.1"
     ),
     channels = c("bioconda", "conda-forge")
 )
