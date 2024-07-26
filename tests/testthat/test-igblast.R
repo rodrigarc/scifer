b4bfcc9bb72ce983f206fb4bff7ac368cd5ba62e
Rodrigo Arcoverde <rodrigo.arcoverdi@gmail.com>
2024-07-26 15:37:01 +0200

    fix test to run igblast on windows

--- tests/testthat/test-igblast.R
+++ tests/testthat/test-igblast.R
@@ -15,7 +15,6 @@ test_that("Database directory inputs", {
         igblast(database = character(0), fasta = system.file("extdata/test_fasta/test_igblast.txt", package = "scifer"), threads = 1),
         "The database directory does not exist.")
     skip_if(basilisk.utils::isMacOSXArm() == TRUE, message = "This test is not supported on MacOSXArm")
-    skip_on_os(os = "windows")
 })
   
 test_that("Fasta file directory inputs", {    
@@ -31,7 +30,6 @@ test_that("Fasta file directory inputs", {
     expect_error(
         igblast(database = system.file("extdata/test_fasta/KIMDB_rm", package = "scifer"), fasta = NULL, threads = 1),
         "The fasta file directory does not exist.")
-    skip_on_os(os = "windows")
     skip_if(basilisk.utils::isMacOSXArm() == TRUE, message = "This test is not supported on MacOSXArm")
 })
 
@@ -52,12 +50,10 @@ test_that("Threads argument inputs", {
         igblast(database = system.file("extdata/test_fasta/KIMDB_rm", package = "scifer"), 
             fasta = system.file("extdata/test_fasta/test_igblast.txt", package = "scifer"), 
             threads = character(0)), "The threads argument should be a numeric value.")
-    skip_on_os(os = "windows")
     skip_if(basilisk.utils::isMacOSXArm() == TRUE, message = "This test is not supported on MacOSXArm")
 })
 
 test_that("returns a data.frame object", {
-    skip_if(basilisk.utils::isWindows() == TRUE, message = "This test is not supported on Windows")
     skip_if(basilisk.utils::isMacOSXArm() == TRUE, message = "This test is not supported on MacOSXArm")
     result <- igblast(
                     database = system.file("extdata/test_fasta/KIMDB_rm", package = "scifer"), 
@@ -65,5 +61,4 @@ test_that("returns a data.frame object", {
                     threads = 1
                     )
     expect_s3_class(result, "data.frame")
-    skip_on_os(os = "windows")
 })
