9a65c2dc83d506c38b7dfcdbfdef8b20306edb95
Rodrigo Arcoverde <rodrigo.arcoverdi@gmail.com>
2024-07-29 14:16:49 +0200

    download igblast within basilisk function
    conditional only for windows users

--- R/basilisk.R
+++ R/basilisk.R
@@ -52,11 +52,36 @@ env_windows <- list(
 # Switch environment
 
 if (basilisk.utils::isWindows()) {
+  # Define the URL and destination path for Windows
+  url_igblast <- "https://ftp.ncbi.nih.gov/blast/executables/igblast/release/LATEST/ncbi-igblast-1.22.0-win64.exe"
+  dest_dir <- system.file("script", package = "scifer")
+  dest_file <- here::here(dest_dir, "ncbi-igblast-1.22.0-win64.exe")
+  print("Running on a Windows machine.")
+  if(BiocBaseUtils::askUserYesNo("Do you want to download the igblast executable?", 
+                                 interactive.only = TRUE)) {
+    # Download the file
+    # Create the destination directory if it doesn't exist
+    if (!dir.exists(dest_dir)) {
+      stop("Destination directory does not exist.")
+    }
+    print(paste0("Downloading the igblast executable to ", dest_file, "..."))
+    download.file(url = url_igblast, destfile = dest_file, mode = "wb")
+  }
+  # Check if the file has been downloaded correctly
+  if (file.exists(dest_file)) {
+    print("Igblast executable downloaded successfully,")
+  } else {
+    stop("Failed to download the igblast executable.\n")
+  }
+  # set the windows environment
   env <- env_windows
-} else if (basilisk.utils::isMacOSXArm()) {
+  } else if (basilisk.utils::isMacOSXArm()) {
+    # set the OSXArm environment to pass the tests
+    # at the momment igblast is not runnable from ARM architecture
   env <- env_osxArm 
   } else if (Sys.info()[["sysname"]] == "Linux"|
            basilisk.utils::isMacOSX()) {
+    # set env for linux and intel MacOS OS
   env <- env_unix
 } else {
   stop("Unsupported operating system or architecture.\n")
